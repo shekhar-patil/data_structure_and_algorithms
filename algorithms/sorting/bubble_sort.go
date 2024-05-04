33c89bbe01c639c90ee9e7f5dfbf46e4034001ea
Shekhar Patil <patilshekhar900@gmail.com>
2024-05-05 01:26:27 +0530

    Add merge sort

--- algorithms/sorting/bubble_sort.go
+++ algorithms/sorting/bubble_sort.go
@@ -2,21 +2,21 @@ package main
 import "fmt"
 
 func BubbleSort(arr []int) {
-	n := len(arr)
-	for i := 0; i < n - 1; i++ {
-		for j := 0; j < n - i - 1; j++ {
-			if arr[j] > arr[j+1] {
-				temp := arr[j]
-				arr[j] = arr[j+1]
-				arr[j+1] = temp
-			}
-		}
-	}
+  n := len(arr)
+  for i := 0; i < n - 1; i++ {
+    for j := 0; j < n - i - 1; j++ {
+      if arr[j] > arr[j+1] {
+        temp := arr[j]
+        arr[j] = arr[j+1]
+        arr[j+1] = temp
+      }
+    }
+  }
 
-	fmt.Println(arr);
+  fmt.Println(arr);
 }
 
 func main() {
-	arr := []int { 10, 2, 5, 3, 1 };
-	BubbleSort(arr)
+  arr := []int { 10, 2, 5, 3, 1 };
+  BubbleSort(arr)
 }
---
+++ algorithms/sorting/merge_sort.go
@@ -0,0 +1,45 @@
+package main
+import "fmt"
+
+func Merge(left []int, right []int) []int {
+	i := 0
+	j := 0
+	result := []int {};
+	for (i < len(left) && j < len(right)) {
+		if left[i] < right[j] {
+			result = append(result, left[i])
+			i++;
+		} else {
+			result = append(result, right[j])
+			j++;
+		}
+	}
+
+	if i < len(left) {
+		result = append(result, left[i:]...)
+	}
+
+	if j < len(right) {
+		result = append(result, right[j:]...)
+	}
+
+	return result;
+}
+
+func MergeSort(arr []int) []int {
+	n := len(arr);
+	mid := n / 2;
+	if n <= 1 {
+		return arr;
+	} 
+
+	left := MergeSort(arr[mid:])
+	right := MergeSort(arr[:mid])
+
+	return Merge(left, right);
+}
+
+func main() {
+  arr := []int { 10, 2, 5, 3, 1, 7, 0 };
+  fmt.Println(MergeSort(arr));
+}
