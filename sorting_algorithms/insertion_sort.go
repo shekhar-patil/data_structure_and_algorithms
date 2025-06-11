c5b67c515ce51dc8653902d5723ff4d45a5c41b9
shekhar-patil <patilshekhar900@gmail.com>
2025-06-12 00:26:01 +0530

    Format the code

--- sorting_algorithms/insertion_sort.go
+++ sorting_algorithms/insertion_sort.go
@@ -1,23 +1,24 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
+func insertionSort(nums []int) {
+	for i := 1; i < len(nums); i++ {
+		key := nums[i]
+		j := i - 1
+
+		for j >= 0 && nums[j] > key {
+			nums[j+1] = nums[j]
+			j--
+		}
+		nums[j+1] = key
+	}
+}
+
 func main() {
-  nums := []int{5, 6, 3, 2, 4, 1}
-
-  for i := 1; i < len(nums); i++ {
-    key := nums[i]
-    j := i - 1
-
-    for j >= 0 && nums[j] > key {
-      nums[j+1] = nums[j]
-      j--
-    }
-
-    nums[j+1] = key
-  }
-
-  fmt.Println(nums)
+	nums := []int{10, 2, 5, 3, 1}
+	insertionSort(nums)
+	fmt.Println(nums)
 }
