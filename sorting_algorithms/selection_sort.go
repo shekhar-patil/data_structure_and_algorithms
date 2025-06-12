4422fc778cd39a4d9acb36a40ad7bbd8052bb432
shekhar-patil <patilshekhar900@gmail.com>
2025-06-13 01:05:41 +0530

    formate code

--- sorting_algorithms/selection_sort.go
+++ sorting_algorithms/selection_sort.go
@@ -4,20 +4,26 @@ import (
 	"fmt"
 )
 
-func main() {
-	nums := []int{5, 6, 3, 8, 2, 1}
+func selectionSort(nums []int) {
+	n := len(nums)
 
-	for i := 0; i < len(nums); i++ {
+	for i := 0; i < n; i++ {
 		minIdx := i
 
-		for j := i + 1; j < len(nums); j++ {
+		for j := i + 1; j < n; j++ {
 			if nums[j] < nums[minIdx] {
 				minIdx = j
 			}
 		}
 
-		nums[minIdx], nums[i] = nums[i], nums[minIdx]
+		if minIdx != i {
+			nums[minIdx], nums[i] = nums[i], nums[minIdx]
+		}
 	}
+}
 
+func main() {
+	nums := []int{5, 6, 3, 4, 2, 1}
+	selectionSort(nums)
 	fmt.Println(nums)
 }
