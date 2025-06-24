f796925b085d73399721accc294cdb3af3cfccad
shekhar-patil <patilshekhar900@gmail.com>
2025-06-25 00:01:32 +0530

    formatting

--- dynamic_programming/kandanes_algorithm.go
+++ dynamic_programming/kandanes_algorithm.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"math"
+  "fmt"
+  "math"
 )
 
 func maxSubArray(nums []int) int {
-	maxSum := math.MinInt64
+  maxSum := math.MinInt64
   sum := 0
 
-	for i := 1; i < len(nums); i++ {
+  for i := 1; i < len(nums); i++ {
     sum += nums[i]
 
     if sum > maxSum {
@@ -19,12 +19,12 @@ func maxSubArray(nums []int) int {
     if sum < 0 {
       sum = 0
     }
-	}
+  }
 
-	return maxSum
+  return maxSum
 }
 
 func main() {
-	arr := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
-	fmt.Println("Maximum Subarray Sum:", maxSubArray(arr))
+  arr := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
+  fmt.Println("Maximum Subarray Sum:", maxSubArray(arr))
 }
