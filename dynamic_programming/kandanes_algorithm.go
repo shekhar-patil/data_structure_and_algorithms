3cb9a682722ef908fd53b2bb88e6c6806d3284a9
shekhar-patil <patilshekhar900@gmail.com>
2025-06-24 23:54:24 +0530

    Add Kandane's algorithm

---
+++ dynamic_programming/kandanes_algorithm.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func maxSubArray(nums []int) int {
+	maxSum := math.MinInt64
+  sum := 0
+
+	for i := 1; i < len(nums); i++ {
+    sum += nums[i]
+
+    if sum > maxSum {
+      maxSum = sum
+    }
+
+    if sum < 0 {
+      sum = 0
+    }
+	}
+
+	return maxSum
+}
+
+func main() {
+	arr := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
+	fmt.Println("Maximum Subarray Sum:", maxSubArray(arr))
+}
